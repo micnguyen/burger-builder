a5e0d361fb8e99368dddd6819b3a41a491cb76f9
Michael Nguyen <contact@micnguyen.net>
2019-07-01 10:28:44 +1000

    Updates

---
+++ precache-manifest.efad307534c4e4d69eeefdeb681f45ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c417ccaa0f9aa0817e911e719ab4ab",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "23d467c895c3a7104b1f",
+    "url": "/burger-builder/static/css/main.2bfb2f24.chunk.css"
+  },
+  {
+    "revision": "cc7eefe62fabd5a623cb",
+    "url": "/burger-builder/static/js/2.1f094a89.chunk.js"
+  },
+  {
+    "revision": "23d467c895c3a7104b1f",
+    "url": "/burger-builder/static/js/main.5ca4425a.chunk.js"
+  },
+  {
+    "revision": "6168d9880ae57e7f6b00",
+    "url": "/burger-builder/static/js/runtime~main.ad75294f.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
