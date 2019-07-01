ce57219845fad8867aa32f1dac897a4282aac37d
Michael Nguyen <contact@micnguyen.net>
2019-07-01 11:51:32 +1000

    Add side drawer

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,10 +1,14 @@
 import React from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
+import classes from "./NavigationItems.css";
 
 const navigationItems = () => (
-  <ul>
-    <li>
-      <a href="dasd">A Link</a>
-    </li>
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>
+      Burger Builder
+    </NavigationItem>
+
+    <NavigationItem link="/">Checkout</NavigationItem>
   </ul>
 );
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
 import classes from "./Toolbar.css";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
     <Logo />
-    <nav>...</nav>
+    <NavigationItems />
   </header>
 );
 
